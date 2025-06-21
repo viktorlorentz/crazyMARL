a77f8796ce1deecbeb4d5e7410d1a2ec1df79599
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-06-21 18:12:29 +0200

    back to two quad

--- train_multiquad_ippo.py
+++ train_multiquad_ippo.py
@@ -125,10 +125,10 @@ def main():
             "obs_noise": 0.0,
             "act_noise": 0.0,
             "max_thrust_range": 0.15,
-            "num_quads": 6,
+            "num_quads": 2,
             "episode_length": 2048,
             "cable_length": 0.5,
-            "payload_mass": 0.05,
+            "payload_mass": 0.01,
         },
         "TOTAL_TIMESTEPS": 3_000_000_000, 
         "NUM_ENVS": 8192, # 16384
