21f1a06fa49460a9f2164e73260e2acea1c5200a
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-06-24 19:13:53 +0200

    adjust reward

--- crazymarl/rewards/multi_quad_reward.py
+++ crazymarl/rewards/multi_quad_reward.py
@@ -60,11 +60,11 @@ def calc_reward(
     taut_reward  = (jp.mean(quad_dists) + jp.mean(quad_heights)) / cfg.cable_length
 
     # angular & linear velocity
-    vel_shaping = jp.maximum(20.0 * er(dis, 20.0), 0.01 ) # low velocity tolerance close to the target
+    vel_shaping = jp.maximum(4.0 * er(dis, 50.0), 0.01 ) # low velocity tolerance close to the target
     ang_norms = jp.linalg.norm(angvels, axis=-1)
     lin_norms = jp.linalg.norm(linvels, axis=-1)
-    safe_linvel = jp.exp(- (0.4 * lin_norms)**4) # no reward for high linear velocities
-    ang_vel_reward      = jp.mean(er(ang_norms, vel_shaping))
+    safe_linvel = jp.exp(- (0.5 * lin_norms)**4) # no reward for high linear velocities
+    ang_vel_reward      = jp.mean(er(ang_norms))
     linvel_quad_reward  = jp.mean(er(lin_norms, vel_shaping) * safe_linvel)
     payload_velocity_reward = er(jp.linalg.norm(payload_linlv), vel_shaping)
 
@@ -74,7 +74,7 @@ def calc_reward(
 
     smooth_penalty    = cfg.reward_coeffs["smooth_action_coef"] * jp.mean(jp.abs(action - last_action)**2)
     thrust_cmds = 0.5 * (action + 1.0)
-    thrust_extremes = jp.exp(-50 * jp.abs(thrust_cmds)) + jp.exp(50 * (thrust_cmds - 1)) # 1 if thrust_cmds is 0 or 1 and going to 0 in the middle
+    thrust_extremes = jp.exp(-50 * jp.abs(thrust_cmds)) + jp.exp(10 * (thrust_cmds - 1)) # 1 if thrust_cmds is 0 or 1 and going to 0 in the middle
     # if actions out of bounds lead them to action space
     thrust_extremes = jp.where(jp.abs(action)> 1.0, 1.0 + 0.1*jp.abs(action), thrust_extremes)  
 
