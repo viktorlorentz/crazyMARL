2a312a4ff16a0010d462b26ac926b4aedf26d9f5
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-08-29 22:41:15 +0200

    allow higher thrust

--- crazymarl/envs/multi_quad_env.py
+++ crazymarl/envs/multi_quad_env.py
@@ -133,10 +133,10 @@ class MultiQuadEnv(PipelineEnv):
         cfg = self.cfg
         rng, mt_rng = jax.random.split(rng)
         motor_offsets = 0.05 * cfg.max_thrust_range * ( jax.random.normal(mt_rng, (self.sys.nu,)))
-        max_thrust = jax.random.uniform(mt_rng, minval=0.11, maxval=0.135)
+        max_thrust = jax.random.uniform(mt_rng, minval=0.11, maxval=0.14)
         max_thrust += motor_offsets
 
-        max_thrust = jp.clip(max_thrust, 0.10, 0.14) 
+        max_thrust = jp.clip(max_thrust, 0.10, 0.15) 
 
         rng, r1, r2, rc = jax.random.split(rng, 4)
         base_qpos = self.sys.qpos0
